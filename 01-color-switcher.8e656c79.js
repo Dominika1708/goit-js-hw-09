46f665c363cba5ec89fc8f4c875057594e21c847
Dominika1708 <99330819+Dominika1708@users.noreply.github.com>
2022-08-08 20:33:50 +0000

    Deploying to gh-pages from @ Dominika1708/goit-js-hw-09@5f0dd427454b3c2d86cd6613c8d528b583ac44c8 🚀

---
+++ 01-color-switcher.8e656c79.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a=null;d.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,a=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.8e656c79.js.map
